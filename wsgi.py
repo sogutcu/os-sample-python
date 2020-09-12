276ea4bca8cdb4907815b987ac99e3459736a81e
sogutcu <sogutcu@gmail.com>
2020-09-12 23:33:01 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,11 @@
-from flask import Flask
 import socket
+import time
+import atexit
+
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
 
 application = Flask(__name__)
 
@@ -7,6 +13,14 @@ application = Flask(__name__)
 def hello():
     return socket.gethostname() + " says 'Hello World!'"
 
+def tick():
+    print(socket.gethostname() + time.strftime(" %A, %d. %B %Y %I:%M:%S %p"))
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=tick, trigger="interval", seconds=5)
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == "__main__":
     application.run()
